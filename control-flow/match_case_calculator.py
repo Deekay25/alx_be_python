7c6287cd98957deec8e0db8a42cc19dc5df8f0b4
adnan ahmad <adnan@MacBook-Pro-2.local>
2025-10-13 19:54:41 +0100

    division

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,37 @@
+# point = (5, 0)
+
+# match point:
+#     case (0, 0):
+#         print("Origin")
+#     case (x, 0):
+#         print(f"Point on X-axis at x={x}")
+#     case (0, y):
+#         print(f"Point on Y-axis at y={y}")
+#     case (x, y):
+#         print(f"Point at x={x}, y={y}")
+
+num1= int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+
+match operator:
+    case '+':
+        result = num1 + num2
+        print("The result is: ",result)
+    case '-':
+        result = num1 - num2
+        print("The result is: ",result)
+    case '*':
+        result = num1 * num2
+        print("The result is: ",result)
+
+    case '/':
+        if num2 == 0:
+            print("Zero division is not available: ")
+        else:
+            result = num1/num2
+            print("The result is: ",result)
+
+    
+    case _:
+        print("Only valid operation")
